c2c1a886e474e84e52bcc8a5fdce4c29660c4437
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-22 20:40:41 +0100

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hola Mundo!(Tarea 02 de C1)");
+            int num1 = 5;
+            int num2 = 6;
+            double numero3 = 2.57;
+            double suma;
+            suma = num1 + numero3;
+
+            Console.WriteLine(suma);
+
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El numero introducido es: " + valor1);
+
+            int valorint = Convert.ToInt32(valor1);
+
+            Console.ReadKey();
+        }
+    }
+}
